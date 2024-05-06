7a4bc7af0f79a4268d7b293f3a832c9d3ba741ad
Lora Seguin <lora80123@gmail.com>
2024-05-06 09:32:18 +0200

    create recipe of me page

---
+++ component/recipe_of_me.tsx
@@ -0,0 +1,152 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+import { Recipe, useAuth } from './authContext';
+import axios from 'axios';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { NavigationProp, ParamListBase } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+interface ConnectedHomeProps {
+    navigation: NavigationProp<ParamListBase>;
+}
+
+
+export default function RecipeOfMe({navigation}: ConnectedHomeProps) {
+    const { user, token } = useAuth();
+    const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+    useEffect(() => {
+        fetchRecipe();
+    }, [token, user]);
+
+    const fetchRecipe = async () => {
+        try {
+            const response = await axios.get(`${process.env.EXPO_PUBLIC_API_URL}/recipes/${user?.id}`, {
+                headers: { 'Authorization': `Bearer ${token}` }
+            });
+            setRecipes(response.data || []);
+        } catch (error) {
+            console.error('Error fetching selection:', error);
+        }
+    };
+
+    const renderItem = ({ item }: { item: Recipe }) => (
+        <View style={styles.card}>
+            <TouchableOpacity onPress={() => navigation.navigate("RecipeDetails", { id: item.id_recipe })}>
+                {item.id_recipe && item.id_recipe.length > 0 ? (
+                    <Image style={styles.cardImage} source={{ uri: item.image }} />
+                ) : (
+                    <View style={styles.cardImagePlaceholder}>
+                        <Text style={styles.cardImagePlaceholderText}>Pas de recettes</Text>
+                    </View>
+                )}
+                <Text style={styles.cardTitle}>{item.name_recipe}</Text>
+            </TouchableOpacity>
+        </View>
+    );
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
+                    <Icon name="arrow-back" size={24} color="#000" />
+                </TouchableOpacity>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>Mes recettes</Text>
+                </View>
+            </View>
+            <FlatList
+                data={recipes}
+                renderItem={renderItem}
+                keyExtractor={item => item.id_recipe}
+                numColumns={2}  // Set up two columns
+                contentContainerStyle={styles.resultsContainer}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 30,
+    },
+    contentContainer: {
+        paddingHorizontal: 10,
+    },
+    box1: {
+        height: "10%",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    title: {
+        fontSize: 28,
+        color: "#F38071",
+        fontWeight: "bold",
+    },
+    titleContainer: {
+        flex: 1, // Takes the remaining space
+        alignItems: 'center', // Center aligns the title text horizontally
+    },
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        paddingHorizontal: 10,
+        width: '100%',
+        height: "10%",
+    },
+    backButton: {
+        marginRight: 10,
+    },
+    titre: {
+        fontSize: 28,
+        color: "#F38071",
+        fontWeight: "bold"
+    },
+    box2: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 20,
+        paddingBottom: 10,
+    },
+    resultsContainer: {
+        paddingHorizontal: 5,
+    },
+    card: {
+        flex: 1,
+        margin: 4,
+        backgroundColor: '#FFF',
+        borderRadius: 8,
+        padding: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 1.41,
+        elevation: 2,
+    },
+    cardImage: {
+        width: '100%',
+        height: 150,
+        borderRadius: 5,
+    },
+    cardImagePlaceholder: {
+        width: '100%',
+        height: 150,
+        backgroundColor: '#eee',
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    cardImagePlaceholderText: {
+        color: '#666',
+        fontSize: 16,
+        textAlign: 'center'
+    },
+    cardTitle: {
+        fontSize: 16,
+        marginTop: 5,
+        fontWeight: 'bold',
+        textAlign: "center"
+    }
+});
